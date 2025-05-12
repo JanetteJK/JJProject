283e97d3146454b3e1ae201e0a3e26e468a08135
janetjk <janetjk@metropolia.fi>
2025-05-12 22:29:04 +0300

    funktiot muutettu flaskiksi

--- Peli/Tiedostot/Python-filut/Funktiot/Funktiot.py
+++ Peli/Tiedostot/Python-filut/Funktiot/Funktiot.py
@@ -1,4 +1,12 @@
 import mysql.connector
+from flask import Flask, request, jsonify, Response
+from flask_cors import CORS
+import json
+
+app = Flask(__name__)
+
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 def get_db_connection():
     return mysql.connector.connect(
@@ -10,35 +18,57 @@ def get_db_connection():
         collation='latin1_swedish_ci',
         autocommit=True
     )
-
+@app.route('/kysymys/<person_id>/<order_no>')
 def hae_kysymys(person_id, order_no):
     conn = get_db_connection()
     cursor = conn.cursor()
-    query = "SELECT question FROM question WHERE person_id = %s AND Order_No = %s"
+    query = f"SELECT question FROM question WHERE person_id = '{person_id}' AND Order_No = '{order_no}'"
     cursor.execute(query, (person_id, order_no))
     kysymys = cursor.fetchone()
     conn.close()
-    return kysymys[0] if kysymys else None
 
+    ans = {
+        "kysymys":kysymys
+    }
+    json_ans = json.dumps(ans)
+    response = Response(response=json_ans, status=200, mimetype="application/json")
+    response.headers["Content-Type"] = "charset=utf-8"
+    return ans
 
+@app.route('/oikea_vastaus/<person_id>')
 def hae_oikea_vastaus(person_id):
     conn = get_db_connection()
     cursor = conn.cursor()
-    query = "SELECT answer FROM answer WHERE person_id = %s AND correct = 1"
+    query = f"SELECT answer FROM answer WHERE person_id = '{person_id}' AND correct = 1"
     cursor.execute(query, (person_id,))
     oikea_vastaus = cursor.fetchone()
     conn.close()
-    return oikea_vastaus[0] if oikea_vastaus else None
+
+    ans = {
+        "oikea":oikea_vastaus
+    }
+    json_ans = json.dumps(ans)
+    response = Response(response=json_ans, status=200, mimetype="application/json")
+    response.headers["Content-Type"] = "charset=utf-8"
+    return ans
 
 
+@app.route('/hae_nimi/<person_id>')
 def hae_asiakkaan_nimi(person_id):
     conn = get_db_connection()
     cursor = conn.cursor()
-    query = "SELECT nimi FROM person WHERE id = %s"
+    query = f"SELECT nimi FROM person WHERE id = '{person_id}'"
     cursor.execute(query, (person_id,))
     nimi = cursor.fetchone()
     conn.close()
-    return nimi[0] if nimi else None
+
+    ans = {
+        "nimi":nimi
+    }
+    json_ans = json.dumps(ans)
+    response = Response(response=json_ans, status=200, mimetype="application/json")
+    response.headers["Content-Type"] = "charset=utf-8"
+    return ans
 
 class Rahapussi:
     def __init__(self):
@@ -50,3 +80,7 @@ class Rahapussi:
 
     def hae_saldo(self):
         return self.raha #palauttaa nykysen saldon kun taso päättyy eli näyttää rahanmäärän
+
+
+if __name__ == '__main__':
+    app.run(use_reloader=True, host='127.0.0.1', port=3000)
