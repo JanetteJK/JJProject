5bc70e20e3fe870f99de313c2065e5c606c92d2b
janetjk <janetjk@metropolia.fi>
2025-05-13 17:10:53 +0300

    pelin alustus toimii

--- Peli/Tiedostot/Pelisivu/PELI.js
+++ Peli/Tiedostot/Pelisivu/PELI.js
@@ -1,39 +1,56 @@
-'use strict'
+'use strict';
 
 /* Pelin alustusfunktio, hakee ensimmäisen asiakkaan nimen ja kysymyksen sekä oikean vastauksen*/
-async function prepGame() {
+async function getCustomer() {
   try {
     const response = await fetch('http://127.0.0.1:3000/hae_nimi/1');
-    console.log("hei sinä")
+    console.log('hei sinä');
     const data = await response.json();
-    console.log(data)
+    console.log(data);
 
-    let nimi = document.querySelector("#asiakas-nimi")
-    nimi.innerHTML = data.nimi
+    let nimi = document.querySelector('#asiakas-nimi');
+    nimi.innerHTML = data.nimi;
+  } catch (error) {
+    console.error('Error fetching data', error);
+  }
+}
 
-    const kys = await fetch('http://127.0.0.1:3000/kysymys/1/1')
+async function getQuestion() {
+  try {
+    const kys = await fetch('http://127.0.0.1:3000/kysymys/1/1');
     const vastaus = await kys.json();
 
-    let kysymys = document.querySelector("#asiakas-kysymys")
-    kysymys.innerHTML = vastaus.kysymys
-  }
-
-
-  catch (error) {
-    console.error('Error fetching data', error)
+    let kysymys = document.querySelector('#asiakas-kysymys');
+    kysymys.innerHTML = vastaus.kysymys;
+  } catch (error) {
+    console.error('Error fetching data', error);
   }
 }
 
 /* Pääpeli, tarkistaa onko annettu vastaus oikea */
 
+async function checkAnswer() {
+  try {
+    const response = await fetch ('http://127.0.0.1:3000/oikea_vastaus/1');
+    const vastaus = await response.json();
+    console.log(vastaus);
+  } catch (error) {
+    console.error('Error fetching data', error);
+  }
+
 /* polku sille mitä tapahtuu jos vastaus on oikein
 *  - palkka +5e
 *  - jos yrityksiä 0 niin palkka +15e */
 
-/* polku sille mitä tapahtuu kun vastaus väärin + tarkistaa että yrityksiä on alle 3 */
+let onButtonClick = function() {
+  let person_id = 1;
+  let order_no = 1;
+  let tries = 0;
+  let max_tries = 3;
 
-/* jos yrityksiä 3 häviö ruutu */
+}
+  /* polku sille mitä tapahtuu kun vastaus väärin + tarkistaa että yrityksiä on alle 3 */
 
-/* loppuruutu kun asiakas 10 vastattu oikein */
+  /* jos yrityksiä 3 häviö ruutu */
 
-prepGame()
+  /* loppuruutu kun asiakas 10 vastattu oikein */
