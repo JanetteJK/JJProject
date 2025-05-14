ae8b3a32e58fac93b818449087ea3998ef7c0af7
janetjk <janetjk@metropolia.fi>
2025-05-14 13:14:36 +0300

    eka yritys pääfunktiole

--- Peli/Tiedostot/Pelisivu/PELI.js
+++ Peli/Tiedostot/Pelisivu/PELI.js
@@ -1,7 +1,6 @@
 'use strict';
 
-
-/* Pelin alustusfunktio, hakee ensimmäisen asiakkaan nimen ja kysymyksen sekä oikean vastauksen*/
+/* hakee ensimmäisen asiakkaan nimen*/
 async function getCustomer(person_id) {
   try {
     const response = await fetch(`http://127.0.0.1:3000/hae_nimi/${person_id}`);
@@ -16,9 +15,11 @@ async function getCustomer(person_id) {
   }
 }
 
+/*hakee asiakkaan kysymyksen id ja order_no perusteella*/
 async function getQuestion(person_id, order_no) {
   try {
-    const kys = await fetch(`http://127.0.0.1:3000/kysymys/${person_id}/${order_no}`);
+    const kys = await fetch(
+        `http://127.0.0.1:3000/kysymys/${person_id}/${order_no}`);
     const vastaus = await kys.json();
 
     let kysymys = document.querySelector('#asiakas-kysymys');
@@ -27,33 +28,85 @@ async function getQuestion(person_id, order_no) {
     console.error('Error fetching data', error);
   }
 }
-getCustomer(1)
-getQuestion(1,1)
-/* Pääpeli, tarkistaa onko annettu vastaus oikea */
 
-/*async function checkAnswer() {
+/*hakee oikean vastauksen tietokannasta*/
+async function getAnswer(person_id) {
   try {
-    const response = await fetch ('http://127.0.0.1:3000/oikea_vastaus/1');
-    const vastaus = await response.json();
-    console.log(vastaus);
+    const answ = await fetch(
+        `http://127.0.0.1:3000/oikea_vastaus/${person_id}`);
+    const vastaus = await answ.json();
+    return vastaus;
   } catch (error) {
     console.error('Error fetching data', error);
   }
+}
+
+/* Palkan lisäys funktio */
+
+function addMoney(rahat) {
+  const palkka = 5;
+  const palauta = rahat + palkka;
+  return palauta;
+}
+
+/* Tipin lisäys funktio */
+
+function addTip(rahat) {
+  const tippi = 15;
+  const palauta = rahat + tippi;
+  return palauta;
+}
+
+let rahat = 0;
+let person_id = 1;
+let order_no = 1;
+let tries = 0;
+let max_tries = 3;
+
+
+/* Pääpeli, tarkistaa onko annettu vastaus oikea */
+const button = document.getElementById('button').value;
+
+window.onload = function() {
+  document.getElementById('rahat').innerHTML = rahat + '€';
+  button.addEventListener('click', onButtonClick);
+};
+
+const onButtonClick = function() {
+  let oikea = getAnswer({person_id});
+  while (tries < max_tries) {
+    if (button === oikea) {
+      if (tries === 0) {
+        addMoney();
+        addTip();
+        person_id += 1;
+        order_no === 1;
+        tries === 0;
+      } else {
+        addMoney();
+        person_id += 1;
+        order_no === 1;
+        tries === 0;
+      }
+    } else {
+      tries += 1;
+      order_no += 1;
+    }
+  }
+  if (tries === 3) {
+
+}
+}
+;
+
+
 
 /* polku sille mitä tapahtuu jos vastaus on oikein
 *  - palkka +5e
 *  - jos yrityksiä 0 niin palkka +15e */
 
-/*let onButtonClick = function() {
-  let person_id = 1;
-  let order_no = 1;
-  let tries = 0;
-  let max_tries = 3;
+/* polku sille mitä tapahtuu kun vastaus väärin + tarkistaa että yrityksiä on alle 3 */
 
-}
+/* jos yrityksiä 3 häviö ruutu */
 
-  /* polku sille mitä tapahtuu kun vastaus väärin + tarkistaa että yrityksiä on alle 3 */
-
-  /* jos yrityksiä 3 häviö ruutu */
-
-  /* loppuruutu kun asiakas 10 vastattu oikein */
+/* loppuruutu kun asiakas 10 vastattu oikein */
